d0a79dabf5acbdb687437b34d49f83398979c292
Mayur Sawant <mayursawant556@gmail.com>
2026-08-05 15:08:38 +0530

    arrays

---
+++ arrays/comparision.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main() {
+int age = 22;
+int _age = 25;
+int *ptr = &age;
+int *_ptr = &_age;
+
+printf("difference = %u\n", ptr -_ptr);
+_ptr = &age;
+printf("comparison = %u\n", ptr == _ptr);
+return 0;}
