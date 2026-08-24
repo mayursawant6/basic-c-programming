1c2a942c938d4ec33fe07e107da88b83eb364959
Mayur Sawant <mayursawant556@gmail.com>
2026-08-25 00:06:35 +0530

    string

---
+++ strings/salting.c
@@ -0,0 +1,16 @@
+// salting is adding of string in the password to secure it
+#include<stdio.h>
+#include<string.h>
+void salting(char pass[]);
+int main(){
+    char mayur[100];
+    scanf("%s",mayur);
+    salting(mayur);
+}
+void salting(char pass[]){
+    char salt[]="123";
+    char newpass[200];
+    strcpy(newpass,pass);
+    strcat(newpass,salt);
+    puts(newpass);
+}
