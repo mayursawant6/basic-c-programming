372fe7bde3c8b52918531c10a84e0062d89f9223
Mayur Sawant <mayursawant556@gmail.com>
2026-08-28 20:22:44 +0530

    dynamic

---
+++ dynamic/calloc.c
@@ -0,0 +1,19 @@
+// calloc()
+// continuous allocation
+// initializes with O
+// ptr = (*int) calloc(5, sizeof(int));
+#include<stdio.h>
+#include <stdlib.h>
+int main() {
+int *ptr;
+ptr=calloc(5,sizeof(int));
+ptr[0] = 1;
+ptr[1] = 3;   
+ptr[2] = 5;
+ptr[3] = 7;
+ptr[4] = 9;
+for(int i=0;i<=4;i++){
+    printf("%d",ptr[i]);
+}
+return 0;
+}
