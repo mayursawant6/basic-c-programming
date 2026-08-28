d65b4d41913e0fcd045d663d80586118c871b50d
Mayur Sawant <mayursawant556@gmail.com>
2026-08-28 23:26:07 +0530

    dynamic

---
+++ dynamic/conceptd.c
@@ -0,0 +1,6 @@
+// Functions for DMA
+// a. malloc() - memory allocator
+// b. calloc()- continous allocator
+// c. free() -free
+// d. realloc() -reallocator
+// include <stblib.h> library for malloc, calloc, free, realloc
